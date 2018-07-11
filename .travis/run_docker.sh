1c5869da69315f2aea848ff8caccfa8e62081823
lyndonhenry <lyndonhenrydotnet@gmail.com>
2018-07-11 12:42:50 +1000

    Wed 11 Jul 12:42:49 AEST 2018

--- .travis/run_docker.sh
+++ .travis/run_docker.sh
@@ -2,26 +2,31 @@
 
 set -ouex pipefail
 
-if [ $# == 0 ];
-then
-    docker run \
-        --volume $(pwd):/home/souffle/souffle \
-        --env "CC=${CC}" \
-        --env "CXX=${CXX}" \
-        --env "SOUFFLE_CONFS=${SOUFFLE_CONFS}" \
-        --env "SOUFFLE_CATEGORY=${SOUFFLE_CATEGORY}" \
-        --env "SOUFFLE_CONFIGURE_OPTIONS=${SOUFFLE_CONFIGURE_OPTIONS}" \
-        --env "SOUFFLE_MAKE_JOBS=${SOUFFLE_MAKE_JOBS}" \
-        souffle/ubuntu:bionic-base \
-        /bin/bash -c "cd souffle && ./.travis/run_docker.sh souffle"
-else
-    ulimit -c unlimited -S
-    ./bootstrap
-    ./configure ${SOUFFLE_CONFIGURE_OPTIONS}
-    make -j${SOUFFLE_MAKE_JOBS}
-    if [[ "$SOUFFLE_CATEGORY" != *"Unit"* ]]
-    then
-      cd tests
-    fi
-    TESTSUITEFLAGS="-j${SOUFFLE_MAKE_JOBS}" make check -j${SOUFFLE_MAKE_JOBS}
-fi
+CC="gcc-7"
+CXX="g++-7"
+
+# CC=clang-4.0
+# CXX=clang++-4.0
+
+SOUFFLE_DOCKER_BASE_IMAGE="souffle/ubuntu:bionic-base"
+SOUFFLE_CATEGORY="FastEvaluation,Interface,Profile,Provenance"
+SOUFFLE_CONFIGURE_OPTIONS="--enable-mpi"
+SOUFFLE_CONFS="-c -j8 -empi"
+SOUFFLE_MAKE_JOBS="2"
+
+SOUFFLE_DOCKER_TAG="$(echo ${SOUFFLE_DOCKER_BASE_IMAGE} | sed 's/-base/-test/g')"
+
+docker build \
+    --tag ${SOUFFLE_DOCKER_TAG} \
+    --build-arg BASE_IMAGE="${SOUFFLE_DOCKER_BASE_IMAGE}" \
+    --build-arg CC="${CC}" \
+    --build-arg CXX="${CXX}" \
+    --build-arg SOUFFLE_CONFS="${SOUFFLE_CONFS}" \
+    --build-arg SOUFFLE_CATEGORY="${SOUFFLE_CATEGORY}" \
+    --build-arg SOUFFLE_CONFIGURE_OPTIONS="${SOUFFLE_CONFIGURE_OPTIONS}" \
+    --build-arg SOUFFLE_MAKE_JOBS="${SOUFFLE_MAKE_JOBS}"
+
+docker run \
+    --interactive \
+    --tty \
+    ${SOUFFLE_DOCKER_TAG}
