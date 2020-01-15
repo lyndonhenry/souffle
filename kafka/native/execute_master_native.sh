15e61f31e416c69dcec9f89e2db45501cd8ad7e2
Kamil Jezek <kamil.jezek@sydney.edu.au>
2020-01-15 16:00:33 +1100

    #1 docker version for kafka native souffle

--- kafka/native/execute_master_native.sh
+++ kafka/native/execute_master_native.sh
@@ -1,36 +1,14 @@
 #!/bin/bash
 
 PROGRAM_NAME=$1
-JSON_DATA=$(echo $(cat ${PROGRAM_NAME}.json))
+JSON_DATA=$(./program -i-2)
+RELATION_NAMES="$(echo ${JSON_DATA} | jq -r '.RelationNames | .[]')"
+STRATUM_NAMES="$(echo ${JSON_DATA} | jq -r '.StratumNames | .[]')"
 OUTPUT_DIR="output"
 INPUT_DIR="facts"
 INPUT_FACTS=$(ls facts/*.facts)
 
 source "$(dirname "$0")/kafka_api.sh"
-
-
-#
-#   Iterate all input files
-#
-function iterate_input_files {
-    local command=$1
-
-    for f in $INPUT_DIR/*.facts; do
-        local TOPIC=$(basename $f | cut -f 1 -d '.')
-
-        $command "IIII${TOPIC}" "$f" 
-    done
-}
-
-#
-#   Read input file and send it to kafka
-#
-function send_input_file {
-    local TOPIC="$1"
-    local FILE="$2"
-
-    send_message_async "${TOPIC}" "${FILE}"
-}
 
 #
 #   Read output data from Kafka messages and store them in files. 
@@ -45,33 +23,53 @@ function read_output_data {
 #   Extend docker compose with strata
 #
 function extend_compose {
-    STRATUM_INDEX="$1"
+    RELATION_NAME="$1"
 
   echo "  
-  strata${STRATUM_INDEX}:
+  strata${RELATION_NAME}:
     image: '${DOCKER_IMAGE}:latest'  
     environment: 
-      STRATUM_INDEX: ${STRATUM_INDEX}  
       KAFKA_HOST: kafka:29092
     networks:
       - app-tier
-    command: ./execute_strata.sh program 
+    command: ./execute_strata_native.sh program "${RELATION_NAME}"
     " >> docker-compose.yml
 
 
 } 
 
+#
+# Execute a call-back method asynchronously
+#
+function for_each_async() {
+    local f="${1}"
+    local xs="${@:2}"
+    local x
+    for x in ${xs}
+    do
+        ${f} ${x} &
+    done
+}
 
 #
-#   Start-up docker for every strata
+# Execute a call-back method 
 #
-# iterate_stratas execute_strata_docker    
+function for_each() {
+    local f="${1}"
+    local xs="${@:2}"
+    local x
+    for x in ${xs}
+    do
+        ${f} ${x}
+    done
+}
+
 
 #
 #   Extend docker compose with images containers that represent stratas
 #
-extend_compose -1
-iterate_stratas extend_compose
+extend_compose "-1"
+for_each extend_compose ${RELATION_NAMES}
 
 #
 #   Start-up the whole environment - Kafka + nodes for Stratas
@@ -83,32 +81,31 @@ docker-compose up -d
 #
 wait_kafka_ready
 
+# ensure that all program specific topics exist
+# Ask Lyndon if we need this
+for_each_async create_topic ${RELATION_NAMES}
+wait  
 
-START=$(date +%s.%N)
+# START=$(date +%s.%N)
 
-echo "Distributing input facts to stratas"
-#
-#   Iterate over input facts and send them into Kafka 
-#   
-iterate_input_files send_input_file
 
-echo "Collecting results from stratas"
-#
-#   Waiting for all results to be ready
-#
-iterate_stratas iterate_output_relations_strata read_output_data
-wait    #   Wait until all results are ready
+# echo "Collecting results from stratas"
+# #
+# #   Waiting for all results to be ready
+# #
+# iterate_stratas iterate_output_relations_strata read_output_data
+# wait    #   Wait until all results are ready
 
-END=$(date +%s.%N)
+# END=$(date +%s.%N)
 
-echo "All results collected in ${OUTPUT_DIR}"
+# echo "All results collected in ${OUTPUT_DIR}"
 
-# docker-compose down
+# # docker-compose down
 
-echo "Output dir content:"
-ls -lh ${OUTPUT_DIR}
+# echo "Output dir content:"
+# ls -lh ${OUTPUT_DIR}
 
-DIFF=$(echo "$END - $START" | bc)
-echo "Total time: $DIFF (Sec.nanos)"
+# DIFF=$(echo "$END - $START" | bc)
+# echo "Total time: $DIFF (Sec.nanos)"
 
 /bin/bash
